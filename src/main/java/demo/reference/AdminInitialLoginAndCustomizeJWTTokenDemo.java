9fb3651bf4a8ce538f9376a91a318fb095ec8cc1
李德辉 <lidehui@id.link>
2020-03-20 00:46:12 +0800

    Add get user info demo

--- src/main/java/demo/reference/AdminInitialLoginAndCustomizeJWTTokenDemo.java
+++ src/main/java/demo/reference/AdminInitialLoginAndCustomizeJWTTokenDemo.java
@@ -1,10 +1,15 @@
 package demo.reference;
 
+import com.google.gson.Gson;
 import demo.tools.AdminApiToken;
 import demo.tools.PropertiesUtils;
 import idlink.ig.client.api.AdminApi;
+import idlink.ig.client.api.AuthenticationApi;
 import idlink.ig.client.model.AdminInitialLoginRequest;
 import io.swagger.client.ApiException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Demo same user login with different clientMetadata and get different id_token claims
@@ -22,6 +27,10 @@ public class AdminInitialLoginAndCustomizeJWTTokenDemo {
      * Admin api secret
      */
     public static String apiSecret = PropertiesUtils.getApiSecret();
+    /**
+     * Authentication api
+     */
+    private static final AuthenticationApi authApi = new AuthenticationApi();
 
     public static void main(String[] args) throws ApiException {
         // Build token before calling each api
@@ -49,5 +58,11 @@ public class AdminInitialLoginAndCustomizeJWTTokenDemo {
                 token.getX_API_CLIENT_ID(), token.getX_API_TIMESTAMP(), token.getX_API_TOKEN());
         System.out.println("\n Login again and get different id_token: ");
         System.out.println(userLoginResult);
+
+        Map userTokens = new Gson().fromJson(userLoginResult, HashMap.class);
+        String authorization = userTokens.get("token_type") + " " + java.util.Base64.getEncoder().encodeToString(userTokens.get("access_token").toString().getBytes());
+        String userInfo = authApi.oauth2UserInfo(authorization);
+        System.out.println("\n Get user info by access Token: ");
+        System.out.println(userInfo);
     }
 }
