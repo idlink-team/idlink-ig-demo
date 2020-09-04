661813e020c3b2f319b035f02284c5240fe546c5
liushuai <liushuai@id.link>
2020-09-04 15:38:20 +0800

    feature: get ReqCode For Ali TMALL By OAuth2

---
+++ src/main/java/demo/reference/oauth/OAuth2LoginForAliByPasswordDemo.java
@@ -0,0 +1,22 @@
+package demo.reference.oauth;
+
+import demo.tools.GetUtils;
+import demo.tools.PropertiesUtils;
+import io.swagger.client.ApiException;
+
+import java.io.IOException;
+
+public class OAuth2LoginForAliByPasswordDemo {
+
+    public static String apiClientId = PropertiesUtils.getApiClientId();
+
+    public static String apiSecret = PropertiesUtils.getApiSecret();
+
+    /**
+     * get ReqCode For Ali TMALL By OAuth2
+     */
+    public static void main(String[] args) throws ApiException, IOException {
+        String result = GetUtils.get("https://api.ig.id.link:1443/oauth2/authorize/codeOnly?response_type=code&client_id=" + apiClientId + "&redirect_uri=www.baidu.com&state=123");
+        System.out.println("result = " + result);
+    }
+}
---
+++ src/main/java/demo/tools/GetUtils.java
@@ -0,0 +1,30 @@
+package demo.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class GetUtils {
+
+    public static String get(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("accept", "*/*");
+        connection.setRequestProperty("connection", "Keep-Alive");
+        connection.setRequestProperty("user-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+        connection.connect();
+
+        String result = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+        String line;
+        while ((line = in.readLine()) != null) {
+            result += line;
+        }
+        if (in != null) {
+            in.close();
+        }
+        return result;
+    }
+}
